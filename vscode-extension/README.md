ccac77c6b24969b96dce5d01a063e254911f12ae
zhihaopan <zhihaopan@tencent.com>
2022-06-06 20:19:32 +0800

    up

---
+++ vscode-extension/README.md
@@ -0,0 +1,8 @@
+mv target `pls` to directory `bin`
+
+build vscode extension
+```shell
+npm install -g vsce
+# with Y
+vsce package --no-yarn
+```
---
+++ vscode-extension/src/extension.ts
@@ -0,0 +1,46 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext) {
+    // The server is implemented in node
+    const serverModule = context.asAbsolutePath(
+        path.join('bin', 'pls')
+    );
+    // let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    const serverOptions: ServerOptions = {
+        run: { command: serverModule, transport: TransportKind.stdio },
+        debug: { command: serverModule, transport: TransportKind.stdio },
+    };
+
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'proto' }],
+    };
+
+    // Create the language client and start the client.
+    let client = new LanguageClient(
+        'proto',
+        'protobuf-language-server',
+        serverOptions,
+        clientOptions
+    );
+
+    // Start the client. This will also launch the server
+    client.start();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
