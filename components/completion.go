ee06abb134c7342d82ac6a609ba7c164a48dd5d7
brotifypacha <brotifypacha@gmail.com>
2024-07-04 09:54:45 +0300

    extract package name completion to a separate function

--- components/completion.go
+++ components/completion.go
@@ -37,28 +37,7 @@ func Completion(ctx context.Context, req *defines.CompletionParams) (*[]defines.
 	word := getWord(line_str, int(req.Position.Character-1), false)
 	if req.Context.TriggerKind != defines.CompletionTriggerKindTriggerCharacter {
 		res, err := CompletionInThisFile(proto_file)
-
-		kindModule := defines.CompletionItemKindModule
-		for _, im := range proto_file.Proto().Imports() {
-			import_uri, err := view.GetDocumentUriFromImportPath(req.TextDocument.Uri, im.ProtoImport.Filename)
-			if err != nil {
-				continue
-			}
-
-			file, err := view.ViewManager.GetFile(import_uri)
-			if err != nil {
-				continue
-			}
-
-			if len(file.Proto().Packages()) > 0 {
-				*res = append(*res, defines.CompletionItem{
-					Label:      file.Proto().Packages()[0].ProtoPackage.Name,
-					Kind:       &kindModule,
-					InsertText: &file.Proto().Packages()[0].ProtoPackage.Name,
-				})
-			}
-
-		}
+		*res = append(*res, GetImportedPackages(proto_file)...)
 
 		return res, err
 	}
@@ -78,6 +57,42 @@ func Completion(ctx context.Context, req *defines.CompletionParams) (*[]defines.
 		}
 	}
 	return nil, nil
+}
+
+func GetImportedPackages(proto_file view.ProtoFile) (res []defines.CompletionItem) {
+	unique := make(map[string]struct{})
+	for _, im := range proto_file.Proto().Imports() {
+		import_uri, err := view.GetDocumentUriFromImportPath(proto_file.URI(), im.ProtoImport.Filename)
+		if err != nil {
+			continue
+		}
+
+		file, err := view.ViewManager.GetFile(import_uri)
+		if err != nil {
+			continue
+		}
+
+		if len(file.Proto().Packages()) == 0 {
+			continue
+		}
+
+		packageName := file.Proto().Packages()[0].ProtoPackage.Name
+
+		if _, exist := unique[packageName]; exist {
+			continue
+		}
+
+		kindModule := defines.CompletionItemKindModule
+		unique[packageName] = struct{}{}
+		res = append(res, defines.CompletionItem{
+			Label:      packageName,
+			Kind:       &kindModule,
+			InsertText: &packageName,
+		})
+
+	}
+
+	return res
 }
 
 func CompletionInThisFile(file view.ProtoFile) (result *[]defines.CompletionItem, err error) {
